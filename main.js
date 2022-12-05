d3615305d8fd381cf156890f0280f7934082de77
MrRio21 <108085558+MrRio21@users.noreply.github.com>
2022-12-05 13:29:23 +0200

    x

---
+++ main.js
@@ -0,0 +1,59 @@
+// function opening() {
+//     Window = window.open("https://www.linkedin.com/feed/","","_blank",);
+// }
+// function closing(){
+//     Window.close();
+// }
+
+////////////////lab2//////////////////
+
+// function getTime(){
+    //     let time = new Date();
+    //     // console.log(time)
+    //     alert(time)
+    // }
+
+    ////////////////lab3//////////////////
+
+// var str = "Mr Rio"
+// var newWin = window.open("","","_blank");
+// var i =0;
+
+// var str2 = setInterval(function(){
+//     newWin.document.write(str[i]);
+//     i++
+//     if (i>str.length) {
+//         clearInterval(str2)
+//     }
+// },1000)
+
+
+/////////////////lab4////////////////////
+// function scorling()
+// {
+    //     if(scrollY>777)
+    //     {
+        //         alert("HI")
+        //     }
+        // }
+        
+/////////////////lab5////////////////////
+
+
+// document.write("<button id='btn'  >New Window</button>");
+// var btn = document.getElementById('btn');
+// btn.addEventListener('click',openwndow);
+// function openwndow(){
+//     var childwndw= window.open('','','_blank');
+//     childwndw.document.write("Hey Im the new window that will close after a while Don't waits for me!!!");
+//     setTimeout(function(){
+//         childwndw.close();
+//     },10000);
+// }
+
+
+/////////////////lab6////////////////////
+
+// function scroooll() {
+//     Window = window.open("scroll.html","_blank",);
+// }
